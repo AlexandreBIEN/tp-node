a06a7c62f97e3208051b73e56e24aa9822f63eef
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2024-03-05 14:03:56 +0100

    added beaufort scale

--- api.js
+++ api.js
@@ -18,13 +18,61 @@ function getDatas(callback) {
                         let date_options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false };
                         var dateFormatted = date.toLocaleString('fr-FR', date_options);
 
+                        let wind_speed = parsedData.current.wind_speed_10m;
+
+                        let beaufort = 0;
+
+                        // get beaufort scale
+                        switch (true) {
+                            case (wind_speed < 1):
+                                beaufort = 0;
+                                break;
+                            case (wind_speed >= 1 && wind_speed <= 5):
+                                beaufort = 1;
+                                break;
+                            case (wind_speed >= 6 && wind_speed <= 11):
+                                beaufort = 2;
+                                break;
+                            case (wind_speed >= 12 && wind_speed <= 19):
+                                beaufort = 3;
+                                break;
+                            case (wind_speed >= 20 && wind_speed <= 28):
+                                beaufort = 4;
+                                break;
+                            case (wind_speed >= 29 && wind_speed <= 38):
+                                beaufort = 5;
+                                break;
+                            case (wind_speed >= 39 && wind_speed <= 49):
+                                beaufort = 6;
+                                break;
+                            case (wind_speed >= 50 && wind_speed <= 61):
+                                beaufort = 7;
+                                break;
+                            case (wind_speed >= 62 && wind_speed <= 74):
+                                beaufort = 8;
+                                break;
+                            case (wind_speed >= 75 && wind_speed <= 88):
+                                beaufort = 9;
+                                break;
+                            case (wind_speed >= 89 && wind_speed <= 102):
+                                beaufort = 10;
+                                break;
+                            case (wind_speed >= 103 && wind_speed <= 117):
+                                beaufort = 11;
+                                break;
+                            default:
+                                beaufort = 12;
+                                break;
+                        }
+
                         let datas = {
                             time : dateFormatted,
-                            wind_speed : parsedData.current.wind_speed_10m + ' ' + parsedData.current_units.wind_speed_10m,
+                            wind_speed : wind_speed + ' ' + parsedData.current_units.wind_speed_10m,
                             wind_direction : parsedData.current.wind_direction_10m + ' ' + parsedData.current_units.wind_direction_10m,
                             wind_gusts : parsedData.current.wind_gusts_10m + ' ' + parsedData.current_units.wind_gusts_10m,
+                            beaufort : beaufort,
                         };
-                        
+
                         callback(datas);
                     } catch (e) {
                         console.error(e.message);
